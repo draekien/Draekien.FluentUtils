43482bd9e9e395a3eb03ed20c5bd73410e76d4e6
William Pei <draekien@users.noreply.github.com>
2021-12-03 22:47:36 +1100

    test(automapper): add dependency injection integration tests

---
+++ tests/FluentUtils.AutoMapper.UnitTests/AddAutoMapperProfileFacts.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentUtils.AutoMapper.Samples.Core.WeatherForecasts.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace FluentUtils.AutoMapper.UnitTests;
+
+public class AddAutoMapperProfileFacts : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public AddAutoMapperProfileFacts(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task ApplicationStarts()
+    {
+        // ARRANGE
+        HttpClient client = _factory.CreateClient();
+
+        // ACT
+        var response = await client.GetFromJsonAsync<IEnumerable<WeatherForecastDto>>("WeatherForecast");
+
+        // ASSERT
+        response.Should().NotBeNull();
+    }
+}
