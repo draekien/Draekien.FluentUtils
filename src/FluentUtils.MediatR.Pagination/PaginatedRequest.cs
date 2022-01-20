67cbb22584d6cd62cb094a33e389809220fda4bd
William Pei <draekien@users.noreply.github.com>
2022-01-20 20:57:06 +1100

    feat(utils-3): add support for .net 5 in fluentutils.mediatr.pagination

--- src/FluentUtils.MediatR.Pagination/PaginatedRequest.cs
+++ src/FluentUtils.MediatR.Pagination/PaginatedRequest.cs
@@ -1,36 +1,37 @@
 ﻿using System.ComponentModel;
 using MediatR;
 
-namespace FluentUtils.MediatR.Pagination;
-
-/// <summary>
-/// The base request object for a paginated API request.
-/// </summary>
-/// <typeparam name="TResult">The return data type for the results IEnumerable.</typeparam>
-public abstract class PaginatedRequest<TResult> : IRequest<PaginatedResponse<TResult>>
+namespace FluentUtils.MediatR.Pagination
 {
-    private readonly int? _limit;
-
     /// <summary>
-    /// Gets or initializes the maximum number of records to return.
+    /// The base request object for a paginated API request.
     /// </summary>
-    [DefaultValue(10)]
-    public int Limit
+    /// <typeparam name="TResult">The return data type for the results IEnumerable.</typeparam>
+    public abstract class PaginatedRequest<TResult> : IRequest<PaginatedResponse<TResult>>
     {
-        get => _limit ?? 10;
-        init
-        {
-            _limit = value switch
-            {
-                < 1 => 10,
-                > 100 => 100,
-                _ => value
-            };
-        }
-    }
+        private readonly int? _limit;
 
-    /// <summary>
-    /// Gets or initializes the offset from 0.
-    /// </summary>
-    public int Offset { get; set; }
+        /// <summary>
+        /// Gets or initializes the maximum number of records to return.
+        /// </summary>
+        [DefaultValue(10)]
+        public int Limit
+        {
+            get => _limit ?? 10;
+            init
+            {
+                _limit = value switch
+                {
+                    < 1 => 10,
+                    > 100 => 100,
+                    _ => value
+                };
+            }
+        }
+
+        /// <summary>
+        /// Gets or initializes the offset from 0.
+        /// </summary>
+        public int Offset { get; set; }
+    }
 }
