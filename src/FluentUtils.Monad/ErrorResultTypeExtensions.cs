bc77039a081590034957852a91f601781633b3e9
william pei <draekien@users.noreply.github.com>
2024-06-26 01:12:36 +1000

    feat: add extensions for generating dynamic error results

---
+++ src/FluentUtils.Monad/ErrorResultTypeExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace FluentUtils.Monad;
+
+/// <summary>
+///     Extensions for <see cref="ErrorResultType{T}" />
+/// </summary>
+public static class ErrorResultTypeExtensions
+{
+    /// <summary>
+    ///     Converts a <see cref="ErrorResultType{T}" /> of type <see cref="TIn" /> to
+    ///     a type of <see cref="TOut" />, preserving the error inside the result
+    /// </summary>
+    /// <param name="result">The <see cref="ErrorResultType{T}" /> to be converted</param>
+    /// <typeparam name="TIn">The input type</typeparam>
+    /// <typeparam name="TOut">The output type</typeparam>
+    /// <returns>A <see cref="ErrorResultType{T}" /> where T is the output type</returns>
+    public static ErrorResultType<TOut> To<TIn, TOut>(
+        this ErrorResultType<TIn> result
+    ) where TIn : notnull where TOut : notnull =>
+        new(result.Error);
+}
--- src/FluentUtils.Monad/Result.cs
+++ src/FluentUtils.Monad/Result.cs
@@ -1,10 +1,13 @@
-﻿using JetBrains.Annotations;
+﻿namespace FluentUtils.Monad;
 
-namespace FluentUtils.Monad;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 
 /// <summary>
-///     A result is the type used for returning and propagating errors. It is a monad with the variants
-///     <see cref="OkResultType{T}" />, representing success and containing some value, and
+///     A result is the type used for returning and propagating errors. It is a
+///     monad with the variants
+///     <see cref="OkResultType{T}" />, representing success and containing some
+///     value, and
 ///     <see cref="ErrorResultType{T}" />,
 ///     representing error and containing an error value.
 /// </summary>
@@ -17,38 +20,104 @@ public static class Result
     /// <param name="value">The success value</param>
     /// <typeparam name="T">The value type</typeparam>
     /// <returns>The created result</returns>
-    public static ResultType<T> Ok<T>(T value) where T : notnull
-    {
-        return new OkResultType<T>(value);
-    }
+    public static ResultType<T> Ok<T>(T value) where T : notnull =>
+        new OkResultType<T>(value);
 
     /// <summary>
     ///     Creates a successful result variant which holds no value
     /// </summary>
     /// <returns>The created result</returns>
-    public static ResultType<Empty> Ok()
-    {
-        return Ok<Empty>(default);
-    }
+    public static ResultType<Empty> Ok() => Ok<Empty>(default);
 
     /// <summary>
     ///     Creates an error result variant
     /// </summary>
     /// <param name="error">The error value</param>
-    /// <typeparam name="T">The value type associated with its success result counterpart</typeparam>
+    /// <typeparam name="T">
+    ///     The value type associated with its success result
+    ///     counterpart
+    /// </typeparam>
     /// <returns>The created result</returns>
-    public static ResultType<T> Error<T>(Error error) where T : notnull
-    {
-        return new ErrorResultType<T>(error);
-    }
+    public static ResultType<T> Error<T>(Error error) where T : notnull =>
+        new ErrorResultType<T>(error);
 
     /// <summary>
-    ///     Creates an error result variant which is the counterpart to a success result that holds no value
+    ///     Creates an error result variant which is the counterpart to a success
+    ///     result that holds no value
     /// </summary>
     /// <param name="error">The error value</param>
     /// <returns>The created result</returns>
-    public static ResultType<Empty> Error(Error error)
+    public static ResultType<Empty> Error(Error error) => Error<Empty>(error);
+
+    /// <summary>
+    ///     Creates an error result variant from a message and an optional exception.
+    /// </summary>
+    /// <remarks>
+    ///     This overload dynamically generates the error code using the name of the
+    ///     calling method / property
+    ///     and the line number where this overload is invoked.
+    /// </remarks>
+    /// <param name="message">The error message to capture inside the result</param>
+    /// <param name="exception">Optional. The exception to capture inside the result</param>
+    /// <param name="caller">The caller member name</param>
+    /// <param name="lineNumber">The caller line number</param>
+    /// <typeparam name="T">
+    ///     The value type associated with its success result
+    ///     counterpart
+    /// </typeparam>
+    /// <returns>The created result</returns>
+    public static ResultType<T> Error<T>(
+        string message,
+        Exception? exception = default,
+        [CallerMemberName] string caller = "",
+        [CallerLineNumber] int lineNumber = 0
+    ) where T : notnull => CreateError(
+        CreateCode(caller, lineNumber),
+        message,
+        exception
+    );
+
+    /// <summary>
+    ///     Creates an error result variant from a message and an optional exception.
+    /// </summary>
+    /// <remarks>
+    ///     This overload dynamically generates the error code using the name of the
+    ///     calling method / property
+    ///     and the line number where this overload is invoked.
+    /// </remarks>
+    /// <param name="message">The error message to capture inside the result</param>
+    /// <param name="exception">Optional. The exception to capture inside the result</param>
+    /// <param name="caller">The caller member name</param>
+    /// <param name="lineNumber">The caller line number</param>
+    /// <returns>The created result</returns>
+    public static ResultType<Empty> Error(
+        string message,
+        Exception? exception = default,
+        [CallerMemberName] string caller = "",
+        [CallerLineNumber] int lineNumber = 0
+    ) => CreateError(
+        CreateCode(caller, lineNumber),
+        message,
+        exception
+    );
+
+    private static Error CreateError(
+        ErrorCode errorCode,
+        string message,
+        Exception? exception
+    ) => new(errorCode, message, exception);
+
+    private static ErrorCode CreateCode(string caller, int lineNumber)
     {
-        return Error<Empty>(error);
+        IEnumerable<char> upperCaseChars = caller.Where(char.IsUpper);
+        string upperCaseCharsString = string.Join("", upperCaseChars);
+        char firstLetter = caller[0];
+
+        return char.IsUpper(firstLetter)
+            ? new ErrorCode($"{upperCaseCharsString}_{lineNumber:D4}")
+            : new ErrorCode(
+                $"{firstLetter}{upperCaseCharsString}_{lineNumber:D4}"
+                   .ToUpperInvariant()
+            );
     }
 }
