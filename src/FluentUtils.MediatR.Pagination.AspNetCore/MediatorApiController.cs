026880f26a4d7de074d42a62d0a1ab6f450c6c0f
William Pei <draekien@users.noreply.github.com>
2024-06-26 16:55:04 +1000

    feat: enable central package management

--- src/FluentUtils.MediatR.Pagination.AspNetCore/MediatorApiController.cs
+++ src/FluentUtils.MediatR.Pagination.AspNetCore/MediatorApiController.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Net.Mime;
-using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace FluentUtils.MediatR.Pagination
+﻿namespace FluentUtils.MediatR.Pagination
 {
+    using System;
+    using System.Net.Mime;
+    using global::MediatR;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
+
     /// <summary>
-    /// An API controller containing Mediator and Pagination.
+    ///     An API controller containing Mediator and Pagination.
     /// </summary>
     [ApiController]
     [Produces(MediaTypeNames.Application.Json)]
@@ -17,55 +17,71 @@ namespace FluentUtils.MediatR.Pagination
         private IMediator? _mediator;
 
         /// <summary>
-        /// Gets the mediator service.
+        ///     Gets the mediator service.
         /// </summary>
-        /// <exception cref="InvalidOperationException">The mediator services has not been registered.</exception>
+        /// <exception cref="InvalidOperationException">
+        ///     The mediator services has not been
+        ///     registered.
+        /// </exception>
         protected IMediator Mediator
         {
             get
             {
                 if (_mediator is not null)
+                {
                     return _mediator;
+                }
 
-                var mediator = HttpContext.RequestServices.GetService<IMediator>();
+                var mediator =
+                    HttpContext.RequestServices.GetService<IMediator>();
 
                 _mediator = mediator
-                         ?? throw new InvalidOperationException("MediatR has not been registered in the current services collection.");
+                            ?? throw new InvalidOperationException(
+                                "MediatR has not been registered in the current services collection."
+                            );
 
                 return _mediator;
             }
         }
 
         /// <summary>
-        /// Creates the navigation links for a paginated API response.
+        ///     Creates the navigation links for a paginated API response.
         /// </summary>
         /// <param name="actionName">The name of the current action.</param>
         /// <param name="request">The paginated API request.</param>
         /// <param name="response">The paginated API response.</param>
-        /// <typeparam name="TResult">The type of the results IEnumerable in the paginated API response.</typeparam>
+        /// <typeparam name="TResult">
+        ///     The type of the results IEnumerable in the paginated
+        ///     API response.
+        /// </typeparam>
         /// <returns>The pagination links.</returns>
         protected Links CreatePaginationLinks<TResult>(
             string actionName,
             PaginatedRequest<TResult> request,
-            PaginatedResponse<TResult> response) => new()
+            PaginatedResponse<TResult> response
+        ) => new()
         {
             Self = CreateLinkRelativeUri(actionName, request),
             Next = CreateNextLink(actionName, request, response),
-            Previous = CreatePreviousLink(actionName, request, response)
+            Previous = CreatePreviousLink(actionName, request, response),
         };
 
         /// <summary>
-        /// Creates the navigation link for the next page of results.
+        ///     Creates the navigation link for the next page of results.
         /// </summary>
         /// <param name="actionName">The name of the current action.</param>
         /// <param name="request">The paginated API request.</param>
         /// <param name="response">The paginated API response.</param>
-        /// <typeparam name="TResult">The type of the results IEnumerable in the paginated API response.</typeparam>
+        /// <typeparam name="TResult">
+        ///     The type of the results IEnumerable in the paginated
+        ///     API response.
+        /// </typeparam>
         /// <returns>The link to the next page if the next page exists.</returns>
         private Uri? CreateNextLink<TResult>(
             string actionName,
             PaginatedRequest<TResult> request,
-            PaginatedResponse<TResult> response)
+            PaginatedResponse<TResult> response
+        )
         {
             if (request.Offset + request.Limit >= response.Total) return null;
 
@@ -79,17 +95,21 @@ namespace FluentUtils.MediatR.Pagination
         }
 
         /// <summary>
-        /// Creates the navigation link for the previous page of results.
+        ///     Creates the navigation link for the previous page of results.
         /// </summary>
         /// <param name="actionName">The name of the current action.</param>
         /// <param name="request">The paginated API request.</param>
         /// <param name="response">The paginated API response.</param>
-        /// <typeparam name="TResult">The type of the results IEnumerable in the paginated API response.</typeparam>
+        /// <typeparam name="TResult">
+        ///     The type of the results IEnumerable in the paginated
+        ///     API response.
+        /// </typeparam>
         /// <returns>The link to the previous page if the previous page exists.</returns>
         private Uri? CreatePreviousLink<TResult>(
             string actionName,
             PaginatedRequest<TResult> request,
-            PaginatedResponse<TResult> response)
+            PaginatedResponse<TResult> response
+        )
         {
             if (request.Offset <= 0) return null;
 
@@ -110,15 +130,29 @@ namespace FluentUtils.MediatR.Pagination
         }
 
         /// <summary>
-        /// Creates the relative URI for the paginated request.
+        ///     Creates the relative URI for the paginated request.
         /// </summary>
         /// <param name="actionName">The name of the current action.</param>
         /// <param name="request">The paginated API request.</param>
-        /// <typeparam name="TResult">The type of the results IEnumerable in the paginated API response.</typeparam>
+        /// <typeparam name="TResult">
+        ///     The type of the results IEnumerable in the paginated
+        ///     API response.
+        /// </typeparam>
         /// <returns>The relative URI.</returns>
-        private Uri CreateLinkRelativeUri<TResult>(string actionName, PaginatedRequest<TResult> request)
+        private Uri CreateLinkRelativeUri<TResult>(
+            string actionName,
+            PaginatedRequest<TResult> request
+        )
         {
-            string action = Url.Action(actionName, request);
+            string? action = Url.Action(actionName, request);
+
+            if (string.IsNullOrWhiteSpace(action))
+            {
+                throw new InvalidOperationException(
+                    $"Cannot create link from action {actionName}"
+                );
+            }
+
             return new Uri(action, UriKind.Relative);
         }
     }
