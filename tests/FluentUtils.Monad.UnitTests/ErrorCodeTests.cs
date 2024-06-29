02c7a0a10da710e557e0d76452d3873e156b4f11
william pei <draekien@users.noreply.github.com>
2024-06-29 15:00:33 +1000

    test: error code

---
+++ tests/FluentUtils.Monad.UnitTests/ErrorCodeTests.cs
@@ -0,0 +1,20 @@
+﻿namespace FluentUtils.Monad.UnitTests;
+
+using FluentAssertions;
+
+public class ErrorCodeTests
+{
+    [Fact]
+    public void
+        GivenString_WhenImplicitlyConvertingToErrorCode_ThenReturnExpectedErrorCode()
+    {
+        // Arrange
+        const string test = nameof(test);
+
+        // Act
+        ErrorCode code = test;
+
+        // Assert
+        code.Should().Be(new ErrorCode(nameof(test)));
+    }
+}
