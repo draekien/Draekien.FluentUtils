f5489a40ea333daf26e3ccb6c68864c20e2465eb
william pei <draekien@users.noreply.github.com>
2024-09-24 22:16:45 +1000

    feat: QOL for pipe when the pipe fn returns a result already

--- src/FluentUtils.Monad/Extensions/PipeAsyncExtensions.cs
+++ src/FluentUtils.Monad/Extensions/PipeAsyncExtensions.cs
@@ -17,7 +17,6 @@ public static class PipeAsyncExtensions
     /// </summary>
     /// <param name="result">The asynchronous <see cref="ResultType{T}" /></param>
     /// <param name="pipeAsync">The pipe operation</param>
-    /// <param name="cancellationToken">The <see cref="CancellationToken" /></param>
     /// <param name="pipeExpression">The pipe async expression</param>
     /// <typeparam name="TIn">The input result value's type</typeparam>
     /// <typeparam name="TOut">The output result value's type</typeparam>
@@ -46,4 +45,26 @@ public static class PipeAsyncExtensions
             },
             Result.ErrorAsync<TOut>
         );
+
+    /// <summary>
+    ///     Pipes the value of an asynchronous <see cref="ResultType{T}" /> to another
+    ///     type if the <see cref="ResultType{T}" />
+    ///     is an <see cref="OkResultType{T}" />, otherwise pipes the
+    ///     <see cref="ErrorResultType{T}" /> to the target type
+    /// </summary>
+    /// <param name="result">The asynchronous <see cref="ResultType{T}" /></param>
+    /// <param name="pipeAsync">The pipe operation</param>
+    /// <param name="pipeExpression">The pipe async expression</param>
+    /// <typeparam name="TIn">The input result value's type</typeparam>
+    /// <typeparam name="TOut">The output result value's type</typeparam>
+    /// <returns>
+    ///     A <see cref="ResultType{T}" /> containing the piped value, or the
+    ///     forwarded <see cref="ErrorResultType{T}" />
+    /// </returns>
+    public static Task<ResultType<TOut>> PipeAsync<TIn, TOut>(
+        this Task<ResultType<TIn>> result,
+        Func<TIn, Task<ResultType<TOut>>> pipeAsync,
+        [CallerArgumentExpression(nameof(pipeAsync))]
+        string pipeExpression =
+            "") => result.MatchAsync(pipeAsync, Result.ErrorAsync<TOut>);
 }
