b593a68c841d5846e20e8465c4294c657dabffda
william pei <draekien@users.noreply.github.com>
2024-09-24 22:47:50 +1000

    feat: tap async

---
+++ src/FluentUtils.Monad/Extensions/TapAsyncExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace FluentUtils.Monad.Extensions;
+
+using System.Runtime.CompilerServices;
+
+public static class TapAsyncExtensions
+{
+    public static Task<ResultType<TIn>> TapAsync<TIn>(
+        this Task<ResultType<TIn>> resultTask,
+        Func<TIn, Task> tapAsync,
+        [CallerArgumentExpression(nameof(tapAsync))]
+        string tapExpression =
+            "") => resultTask.MatchAsync(
+        async value =>
+        {
+            try
+            {
+                await tapAsync(value);
+                return await resultTask;
+            }
+            catch (Exception ex)
+            {
+                return await Result.ErrorAsync<TIn>(
+                    MonadErrors.FailedToTapValue(ex, tapExpression));
+            }
+        },
+        Result.ErrorAsync<TIn>);
+}
